9be88cb483337a4ba0838f919cdc206f133a3f21
Ole Henning <oahenning@googlemail.com>
2021-12-17 17:01:47 +0100

    Some Docstrings

--- components/Chart.vue
+++ components/Chart.vue
@@ -2,6 +2,7 @@
 export default {
   props: ["priceData", "dividendsData", "stockName"],
   methods: {
+    // getOptions returns apex charts options property
     getOptions() {
       return {
         xaxis: {
@@ -19,24 +20,32 @@ export default {
         },
       };
     },
+    // getSeries returns apex charts series property
     getSeries() {
+      // Apex charts data models
       interface SeriesDataPoint {
         x: string;
         y: number;
       }
       const stockChart: SeriesDataPoint[] = [];
       const stockChartWithDividends: SeriesDataPoint[] = [];
-
+      // Calculate growth rate relative to previous node
       for (let i = 0; i < this.priceData.length; i++) {
         this.priceData[i].percentageChangeToPrev =
           this.priceData[i].Close / this.priceData[i - 1]?.Close || 1;
       }
+
+      // Prepare stock price and dividends data in order to calculate total return
       const fullData = this.priceData
         .concat(this.dividendsData)
         .sort((a, b) => Date.parse(a.Date) - Date.parse(b.Date));
+
+      // Define dynamic data
       let valueWithDividends = 1,
         value = 1,
         pricePerStock = this.priceData[0].Close;
+
+      // Calculate stock price and total return growth
       fullData.forEach((e) => {
         if (e.Dividends) {
           valueWithDividends +=
@@ -45,6 +54,7 @@ export default {
           pricePerStock = e.Close;
           value *= e.percentageChangeToPrev;
           valueWithDividends *= e.percentageChangeToPrev;
+          // Push to apex charts data models
           stockChart.push({ x: e.Date, y: value });
           stockChartWithDividends.push({ x: e.Date, y: valueWithDividends });
         }
